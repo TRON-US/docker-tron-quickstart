d82399a9e77f78849db115054766010617c37bad
sullof <sullof@gmail.com>
2018-10-22 12:46:16 -0700

    Exposing sh folder with utilities

---
+++ sh/verifyShasta.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget -qO- https://api.shasta.trongrid.io/wallet/getnowblock
+wget -qO- https://api.shasta.trongrid.io/walletsolidity/getnowblock
+wget -qO- https://api.shasta.trongrid.io
