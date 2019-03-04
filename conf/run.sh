bec8e0c4fc35224ebeccec58aa30284866d3754d
Francesco Sullo <sullof@gmail.com>
2019-03-04 13:58:25 -0800

    Simplifying run.sh

--- conf/run.sh
+++ conf/run.sh
@@ -1,31 +1,4 @@
 #!/usr/bin/env bash
 
-source ./.env
+nohup java -jar BlockParser.jar --Node-list 127.0.0.1 --intial-block 1 -end -1 --event-server http://127.0.0.1:8060 --secret-key TNSpckEZhGfZ4ryidHG2fYWMARLpZ6U139  >/dev/null 2>&1 &
 
-STARTBLOCK=-1
-
-if [[ $TRONEVENTS == "" ]]; then
-  TRONEVENTS=http://127.0.0.1:8060
-fi
-
-if [[ $NODEHOST == "" ]]; then
-  NODEHOST=127.0.0.1
-fi
-
-if [[ $1 != "" ]]; then
-  STARTBLOCK=$1
-fi
-
-ENDBLOCK=-1
-
-if [[ $2 != "" ]]; then
-  ENDBLOCK=$2
-fi
-
-nohup java -jar BlockParser.jar --Node-list $NODEHOST --intial-block $STARTBLOCK -end $ENDBLOCK --event-server $TRONEVENTS  --secret-key $SECRETKEY  >/dev/null 2>&1 &
-
-#if [[ $3 == "" ]]; then
-#  tail -f logs/tron.log
-#fi
-
-echo $TRONEVENTS > currentNodeHost
