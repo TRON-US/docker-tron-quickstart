0be502064f95cf8ae67996d98eca6ac54c5f36c2
Francesco Sullo <sullof@gmail.com>
2019-03-14 14:00:35 -0700

    Remove test folder, for now

--- test/metacoin-box/README.md
+++
@@ -1,4 +0,0 @@
-# MetaCoin TronBox
-
-This version of the MetaCoin example is modified in order to test Tron Quickstart using TronBox.
-
--- test/metacoin-box/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var MetaCoin = artifacts.require("./MetaCoin.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(MetaCoin, 10000);
-};
